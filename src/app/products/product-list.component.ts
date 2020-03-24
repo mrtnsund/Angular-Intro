9a96b7daaa6a11504b614cc5cf96fde0b0e819c5
Morten <mrtnsund@gmail.com>
2020-03-24 12:35:48 +0100

    modul 10 ferdig

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -13,6 +13,7 @@ export class ProductListComponent implements OnInit {
   imageWidth: number = 50;
   imageMargin: number = 2;
   showImage: boolean = false;
+  errorMessage: string;
 
   _listFilter: string;
   get listFilter(): string {
@@ -40,8 +41,13 @@ export class ProductListComponent implements OnInit {
     this.showImage = !this.showImage
   }
   ngOnInit(): void {
-    this.products = this.productService.getProducts();
-    this.filteredProducts = this.products;
+    this.productService.getProducts().subscribe({
+      next: products => {
+        this.products = products
+        this.filteredProducts = this.products
+      },
+      error: err => this.errorMessage = err
+    });
 
   }
   onRatingClicked(message: string): void {
